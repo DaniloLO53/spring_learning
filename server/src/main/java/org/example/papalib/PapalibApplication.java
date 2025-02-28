abe30622dc3166cf9aed62fab0d973c88c00770d
DaniloLO53 <daniloleaodev@gmail.com>
2025-02-27 23:50:10 -0300

    init: spring project

---
+++ server/src/main/java/org/example/papalib/PapalibApplication.java
@@ -0,0 +1,13 @@
+package org.example.papalib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapalibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapalibApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/org/example/papalib/PapalibApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.papalib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapalibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
