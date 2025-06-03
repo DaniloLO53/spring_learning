024ab36ad42cbf0c954d0bb8b6f2267a08fd126c
DaniloLO53 <daniloleaodev@gmail.com>
2025-06-03 08:16:31 -0300

    one-to-one

---
+++ src/main/java/org/example/project/models/SocialUser.java
@@ -0,0 +1,13 @@
+package org.example.project.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class SocialUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(mappedBy = "socialUser")
+    private SocialProfile socialProfile;
+}
