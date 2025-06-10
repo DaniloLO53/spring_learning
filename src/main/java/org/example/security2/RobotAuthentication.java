3f7594f2174f9549ce0593c2fafb13ca09f335ef
DaniloLO53 <daniloleaodev@gmail.com>
2025-06-10 13:14:50 -0300

    security

---
+++ src/main/java/org/example/security2/RobotAuthentication.java
@@ -0,0 +1,44 @@
+package org.example.security2;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+import java.util.Collection;
+
+public class RobotAuthentication implements Authentication {
+    @Override
+    public String getName() {
+        return "I'm Danilo";
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return getName();
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return true;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+        System.out.println("Don't do that");
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return AuthorityUtils.createAuthorityList("ROLE_robot");
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return null;
+    }
+}
---
+++ src/main/java/org/example/security2/controllers/HelloController.java
@@ -0,0 +1,20 @@
+package org.example.security2.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sec")
+public class HelloController {
+    @GetMapping("/public")
+    public String getHello() {
+        return "Hello, public!";
+    }
+
+    @GetMapping("/private")
+    public String getPrivate(Authentication auth) {
+        return "This is private! Hello, " + auth.getName();
+    }
+}
